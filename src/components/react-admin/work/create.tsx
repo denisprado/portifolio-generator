4389b262646a27bc99f71e8c8ffd36b4a1533b87
Denis Prado Forigo <denisforigo@gmail.com>
2023-08-29 22:16:40 -0300

    building

--- src/components/react-admin/work/create.tsx
+++ src/components/react-admin/work/create.tsx
@@ -4,10 +4,10 @@ export const WorkCreate = () => (
 	<Create>
 		<SimpleForm>
 			<TextInput source="title" validate={[required()]} fullWidth />
-			{/* <MediaLibraryInput source="image_1" label="Imagem 1" /> */}
+			<ImageInput source="image_1" label="Imagem 1" />
 			<TextInput source="description_1" validate={[required()]} fullWidth />
 			<TextInput source="tech_description_1" validate={[required()]} fullWidth />
-			{/* <MediaLibraryInput source="image_2" label="Imagem 1" /> */}
+			<ImageInput source="image_2" label="Imagem 1" />
 			<TextInput source="description_2" validate={[required()]} fullWidth />
 			<TextInput source="tech_description_2" validate={[required()]} fullWidth />
 		</SimpleForm>
