23044345814d1ad231c3920a87459b41b08eff90
Denis Forigo <denisforigo@gmail.com>
2023-09-05 15:08:56 -0300

    add cors

--- src/components/react-admin/work/list.tsx
+++ src/components/react-admin/work/list.tsx
@@ -1,5 +1,5 @@
 
-import { Datagrid, EditButton, ImageField, List as ReactAdminList, ShowButton, TextField } from 'react-admin'
+import { Datagrid, EditButton, ImageField, List as ReactAdminList, RichTextField, ShowButton, TextField } from 'react-admin'
 
 
 export const WorkList = () => {
@@ -10,13 +10,13 @@ export const WorkList = () => {
 				<TextField source="title" />
 
 				<ImageField source="image_1" src="image_1" />
-				<TextField source="description_1" />
-				<TextField source="tech_description_1" />
+				<RichTextField source="description_1" />
+				<RichTextField source="tech_description_1" />
 
 
 				<ImageField source="image_2" />
-				<TextField source="description_2" />
-				<TextField source="tech_description_2" />
+				<RichTextField source="description_2" />
+				<RichTextField source="tech_description_2" />
 
 				<EditButton label='Editar' />
 				<ShowButton label='Visualizar' />
