6338d9948d75b2d8ab337b0b7400395f5f600a5d
Denis Forigo <denisforigo@gmail.com>
2023-09-05 10:12:38 -0300

    react-admin as subpath

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+
 import SupabaseProvider from './supabase-provider';
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
 import { PropsWithChildren } from 'react';
 import 'styles/main.css';
+import { usePathname } from 'next/navigation';
 
 const meta = {
 	title: 'Next.js Subscription Starter',
@@ -44,19 +46,12 @@ export default function RootLayout({
 	// This will be populated with nested layouts or pages
 	children
 }: PropsWithChildren) {
+
 	return (
 		<html lang="en">
 			<body className="bg-black loading">
 				<SupabaseProvider>
-					{/* @ts-expect-error */}
-					<Navbar />
-					<main
-						id="skip"
-						className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
-					>
 						{children}
-					</main>
-					<Footer />
 				</SupabaseProvider>
 			</body>
 		</html>
